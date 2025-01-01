ec8d348412ffadaebb1df38f182ee7710c3aecd0
Adnan <adnanali23189@gmail.com>
2025-01-01 15:54:13 +0530

    Rectify code for Linux env

--- test.c
+++ test.c
@@ -1,35 +1,23 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(){
+
     FILE *fp1, *fp2;
     char ch;
-
-    // Open the source file for reading
-    fp1 = fopen("./assets/File1.txt", "r");
-    if (fp1 == NULL) {
-        printf("Unable to open source file.\n");
+    fp1 = fopen("./assets/File1.txt","r");
+    fp2 = fopen("./system_generated_files/Problem1.txt","w");
+    if (fp1 == NULL || fp2 == NULL){
+        printf("Eror in opening file\n");
         exit(1);
     }
 
-    // Open the destination file for writing
-    fp2 = fopen("copied.txt", "w");
-    if (fp2 == NULL) {
-        printf("Unable to create or open destination file.\n");
-        fclose(fp1);
-        exit(1);
+    while((ch=fgetc(fp1))!= EOF){
+        fputc(ch,fp2);
     }
-
-    // Read each character from source and write to destination
-    while ((ch = fgetc(fp1)) != EOF) {
-        fputc(ch, fp2);
-    }
-
-    printf("File copied successfully.\n");
-
-    // Close both files
     fclose(fp1);
     fclose(fp2);
 
-    return 0;
+    return 0;
 }
+
